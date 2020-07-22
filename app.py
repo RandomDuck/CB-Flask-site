b3ae4b1aa330fa18f6d4769e781806af25a4e048
Johannes Ström <schulze190@gmail.com>
2020-07-22 11:09:00 +0200

    segregated code

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask,render_template
 app=Flask(__name__)
 
+@app.before_request
+def before_request():
+    app.jinja_env.cache = {}
+
 @app.route("/")
 def index():
-    return render_template("main.html")
+    return render_template("leaderboard.html")
+
+if __name__ == '__main__':
+    app.run()
